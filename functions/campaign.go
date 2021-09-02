a238296a9c5cbe3136c40e66047e5936b2c66f97
einemutigeschlacht <eine.schlacht@gmail.com>
2021-09-02 15:00:52 +0700

    :zap: update: add strings.Replace in campaign.go file for cut string.

--- functions/campaign.go
+++ functions/campaign.go
@@ -5,13 +5,15 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
-func NewRequestCampaign(mobile_number, campaign_code string) (string, error) {
-	campaign_url := "https://gift.truemoney.com/campaign/vouchers/" + campaign_code + "/redeem"
+func NewRequestCampaign(mobile_number, campaign_link string) (string, error) {
+	campaign_code := strings.Replace(campaign_link, "https://gift.truemoney.com/compaign?v=", "", -1)
+	redeem_url := "https://gift.truemoney.com/campaign/vouchers/" + campaign_code + "/redeem"
 	payload, _ := json.Marshal(map[string]string{"mobile": mobile_number})
 	reader_buffer := bytes.NewBuffer(payload)
-	response_campaign, err := http.Post(campaign_url, "application/json", reader_buffer)
+	response_campaign, err := http.Post(redeem_url, "application/json", reader_buffer)
 	if err != nil {
 		return "", err
 	}
