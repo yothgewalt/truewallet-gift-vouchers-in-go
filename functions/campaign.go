a70bf12e6afb23297dc957e8393d6e4ca7099271
einemutigeschlacht <eine.schlacht@gmail.com>
2021-09-04 22:52:41 +0700

    :zap: update: remove variable of buffers.:

--- functions/campaign.go
+++ functions/campaign.go
@@ -12,8 +12,7 @@ func NewRequestCampaign(mobile_number, campaign_link string) (string, error) {
 	campaign_code := strings.Replace(campaign_link, "https://gift.truemoney.com/compaign?v=", "", -1)
 	redeem_url := "https://gift.truemoney.com/campaign/vouchers/" + campaign_code + "/redeem"
 	payload, _ := json.Marshal(map[string]string{"mobile": mobile_number})
-	reader_buffer := bytes.NewBuffer(payload)
-	response_campaign, err := http.Post(redeem_url, "application/json", reader_buffer)
+	response_campaign, err := http.Post(redeem_url, "application/json", bytes.NewBuffer(payload))
 	if err != nil {
 		return "", err
 	}
